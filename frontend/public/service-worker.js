6421d13871c92e90cdf6222739715b6b0e425382
Abhishek Sawant <abhishakesawant@gmail.com>
2022-07-15 01:24:27 +0530

    removed service-worker code

--- frontend/public/service-worker.js
+++ frontend/public/service-worker.js
@@ -1,57 +1,57 @@
-/* eslint-disable no-restricted-globals */
-const CACHE_NAME = "pwa-task-manager";
-const assetsToCache = ["/", "/chats"];
+// /* eslint-disable no-restricted-globals */
+// const CACHE_NAME = "pwa-task-manager";
+// const assetsToCache = ["/", "/chats"];
 
-// self : ServiceWorkerGlobalScope
+// // self : ServiceWorkerGlobalScope
 
-// Install a service worker
-self.addEventListener("install", (e) => {
-  // Perform install steps
-  e.waitUntil(
-    caches.open(CACHE_NAME).then((cache) => {
-      cache.addAll(assetsToCache);
-    })
-  );
-  // To instantly activate a newly installed service worker
-  self.skipWaiting();
-});
+// // Install a service worker
+// self.addEventListener("install", (e) => {
+//   // Perform install steps
+//   e.waitUntil(
+//     caches.open(CACHE_NAME).then((cache) => {
+//       cache.addAll(assetsToCache);
+//     })
+//   );
+//   // To instantly activate a newly installed service worker
+//   self.skipWaiting();
+// });
 
-// Cache and return requests
-self.addEventListener("fetch", (e) => {
-  e.respondWith(
-    caches.match(e.request).then((res) => {
-      return (
-        res ||
-        fetch(e.request).catch((err) => {
-          console.log("Error occured white fetching : ", err);
-        })
-      );
-    })
-  );
-});
+// // Cache and return requests
+// self.addEventListener("fetch", (e) => {
+//   e.respondWith(
+//     caches.match(e.request).then((res) => {
+//       return (
+//         res ||
+//         fetch(e.request).catch((err) => {
+//           console.log("Error occured white fetching : ", err);
+//         })
+//       );
+//     })
+//   );
+// });
 
-// Update a service worker
-self.addEventListener("activate", async (e) => {
-  const cacheWhitelist = ["pwa-task-manager"];
-  e.waitUntil(
-    caches.keys().then((cacheNames) => {
-      return Promise.all(
-        cacheNames.map((cacheName) => {
-          if (cacheWhitelist.indexOf(cacheName) === -1) {
-            return caches.delete(cacheName);
-          }
-          return Promise.resolve();
-        })
-      );
-    })
-  );
-  // Refresh all tabs
-  const tabs = await self.clients.matchAll({ type: "window" });
-  tabs.forEach((tab) => {
-    try {
-      tab.navigate(tab.url);
-    } catch (error) {
-      console.log("Error occurred while refreshing tab : ", tab.url);
-    }
-  });
-});
+// // Update a service worker
+// self.addEventListener("activate", async (e) => {
+//   const cacheWhitelist = ["pwa-task-manager"];
+//   e.waitUntil(
+//     caches.keys().then((cacheNames) => {
+//       return Promise.all(
+//         cacheNames.map((cacheName) => {
+//           if (cacheWhitelist.indexOf(cacheName) === -1) {
+//             return caches.delete(cacheName);
+//           }
+//           return Promise.resolve();
+//         })
+//       );
+//     })
+//   );
+//   // Refresh all tabs
+//   const tabs = await self.clients.matchAll({ type: "window" });
+//   tabs.forEach((tab) => {
+//     try {
+//       tab.navigate(tab.url);
+//     } catch (error) {
+//       console.log("Error occurred while refreshing tab : ", tab.url);
+//     }
+//   });
+// });
