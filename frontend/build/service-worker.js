a021b102568493321c04ca1d4ac0bade81933f61
Abhishek Sawant <abhishakesawant@gmail.com>
2022-07-15 03:20:35 +0530

    refactor: changed build folder

--- frontend/build/service-worker.js
+++ frontend/build/service-worker.js
@@ -7,3 +7,19 @@ self.addEventListener("install", (e) => {
   // To instantly activate a newly installed service worker
   self.skipWaiting();
 });
+
+// Cache and return requests
+self.addEventListener("fetch", (e) => {});
+
+// Update a service worker
+self.addEventListener("activate", async (e) => {
+  // Refresh all tabs
+  const tabs = await self.clients.matchAll({ type: "window" });
+  tabs.forEach((tab) => {
+    try {
+      tab.navigate(tab.url);
+    } catch (error) {
+      console.log("Error occurred while refreshing tab : ", tab.url);
+    }
+  });
+});
